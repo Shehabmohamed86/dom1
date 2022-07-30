c4ce7edc36f6f0402a0e1bf4bca937865ecf3107
Shehabmohamed86 <shehabmohamed8688@gmail.com>
2022-07-29 21:59:00 -0400

    dom

--- dom1.js
+++ dom1.js
@@ -5,3 +5,19 @@ const newh2 = document.createElement('h2')
 newh2.innerText = 'i love coding'
 newh2.classList.toggle('special')
 form.appendChild(newh2)
+
+const ul = document.querySelector('ul')
+const lastLi = document.querySelectorAll('li')[2]
+const newLi = document.createElement('li')
+newLi.innerText = 'im a new li'
+ul.insertBefore(newLi,lastLi)
+
+const fristP = document.querySelector('p')
+const bt = document.createElement('button')
+const a = document.createElement('a')
+a.innerText = 'im a link!!!'
+fristP.append(bt,a)
+fristP.prepend(bt,a)
+
+const deleted =  ul.removeChild(newLi)
+fristP.remove()
